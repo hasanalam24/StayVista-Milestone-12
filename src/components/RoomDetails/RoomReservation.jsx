e68cae342923c579f8a831fa7ca443bb8ba9287e
hasanalam24 <hasanalam365@gmail.com>
2024-05-29 23:31:53 +0600

    total days count use date.fns.org website

--- src/components/RoomDetails/RoomReservation.jsx
+++ src/components/RoomDetails/RoomReservation.jsx
@@ -2,10 +2,11 @@ import PropTypes from 'prop-types'
 import Button from '../Shared/Button/Button'
 import { useState } from 'react'
 import { DateRange } from 'react-date-range';
+import { differenceInCalendarDays } from 'date-fns';
 
 const RoomReservation = ({ room }) => {
 
-  console.log(new Date(room.to).toLocaleDateString())
+  // console.log(new Date(room.to).toLocaleDateString())
 
   const [state, setState] = useState([
     {
@@ -14,11 +15,19 @@ const RoomReservation = ({ room }) => {
       key: 'selection'
     }
   ]);
+
+  //calender days count (boro date theke choto date minus kore dibe se)  use date-fns.org website
+  const totalDays = parseInt(differenceInCalendarDays(
+    new Date(room.to), new Date(room.from)
+  ))
+  const totalPrice = totalDays * room.price
+  // console.log(totalDays, totalPrice)
+
   return (
     <div className='rounded-xl border-[1px] border-neutral-200 overflow-hidden bg-white'>
       <div className='flex items-center gap-1 p-4'>
         <div className='text-2xl font-semibold'>$ {room?.price}</div>
-        <div className='font-light text-neutral-600'>night</div>
+        <div className='font-light text-neutral-600'>/night</div>
       </div>
       <hr />
       <div className='flex justify-center'>{/* Calender */}
@@ -48,7 +57,7 @@ const RoomReservation = ({ room }) => {
       <hr />
       <div className='p-4 flex items-center justify-between font-semibold text-lg'>
         <div>Total</div>
-        <div>${room?.price}</div>
+        <div>${totalPrice}</div>
       </div>
     </div>
   )
