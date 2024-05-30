0cd62237bd47b9ab85d6e5ef5e14ec91621853f9
hasanalam24 <hasanalam365@gmail.com>
2024-05-30 23:23:07 +0600

    conditinally host menu show and hide

--- src/components/Dashboard/Sidebar/Sidebar.jsx
+++ src/components/Dashboard/Sidebar/Sidebar.jsx
@@ -12,6 +12,7 @@ import { MdHomeWork } from 'react-icons/md'
 import useAuth from '../../../hooks/useAuth'
 import useRole from '../../../hooks/useRole'
 import MenuItem from './Menu/MenuItem'
+import HostMenu from './Menu/HostMenu'
 
 const Sidebar = () => {
     const { logOut } = useAuth()
@@ -97,41 +98,12 @@ const Sidebar = () => {
                                 <span className='mx-4 font-medium'>Statistics</span>
                             </NavLink> */}
 
-                            {/* Add Room */}
-                            <MenuItem label="Add Room"
-                                address='add-room'
-                                icon={BsFillHouseAddFill}
-                            ></MenuItem>
-                            {/* <NavLink
-                                to='add-room'
-                                className={({ isActive }) =>
-                                    `flex items-center px-4 py-2 my-5  transition-colors duration-300 transform  hover:bg-gray-300   hover:text-gray-700 ${isActive ? 'bg-gray-300  text-gray-700' : 'text-gray-600'
-                                    }`
-                                }
-                            >
-                                <BsFillHouseAddFill className='w-5 h-5' />
-
-                                <span className='mx-4 font-medium'>Add Room</span>
-                            </NavLink> */}
+                            {/* Add Room & My Listings */}
+                            {
+                                role === 'host' && <HostMenu></HostMenu>
+                            }
 
 
-                            {/* My Listing */}
-                            <MenuItem label="My Listings"
-                                address='my-listings'
-                                icon={MdHomeWork}
-                            ></MenuItem>
-
-                            {/* <NavLink
-                                to='my-listings'
-                                className={({ isActive }) =>
-                                    `flex items-center px-4 py-2 my-5  transition-colors duration-300 transform  hover:bg-gray-300   hover:text-gray-700 ${isActive ? 'bg-gray-300  text-gray-700' : 'text-gray-600'
-                                    }`
-                                }
-                            >
-                                <MdHomeWork className='w-5 h-5' />
-
-                                <span className='mx-4 font-medium'>My Listings</span>
-                            </NavLink> */}
                         </nav>
                     </div>
                 </div>
